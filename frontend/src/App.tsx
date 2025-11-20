dbc3217de8768a4b801a1036c6109461ffb5cd1b
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2025-11-20 19:14:01 +0000

    login and register forms

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Nav from "./components/nav";
+import Register from './pages/register';
 
 function App() {
   useEffect(() => {
@@ -14,10 +15,11 @@ function App() {
   return (
     <>
       <BrowserRouter>
-      <Nav/>
+        <Nav />
         <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/Login" element={<Login/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ frontend/src/pages/register.tsx
@@ -0,0 +1,54 @@
+export default function Register() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100 p-6 -mt-16">
+            <div className="w-full max-w-md bg-white p-8 rounded-2xl shadow-xl">
+                <h1 className="text-2xl font-semibold text-center mb-6 text-accent">
+                    Register
+                </h1>
+                <form className="space-y-5">
+                    <div>
+                        <label className="block text-sm font-medium mb-1 text-accent">
+                            Firstname
+                        </label>
+                        <input
+                            type="text"
+                            className="w-full px-4 py-2 border rounded-xl focus:outline-none focus:ring focus:ring-accent"
+                        />
+                    </div>
+                    <div>
+                        <label className="block text-sm font-medium mb-1 text-accent">
+                            Lastname
+                        </label>
+                        <input
+                            type="text"
+                            className="w-full px-4 py-2 border rounded-xl focus:outline-none focus:ring focus:ring-accent"
+                        />
+                    </div>
+
+                    <div>
+                        <label className="block text-sm font-medium mb-1 text-accent">
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            className="w-full px-4 py-2 border rounded-xl focus:outline-none focus:ring focus:ring-accent"
+                        />
+                    </div>
+                    <div>
+                        <label className="block text-sm font-medium mb-1">Password</label>
+                        <input
+                            type="password"
+                            className="w-full px-4 py-2 border rounded-xl focus:outline-none focus:ring focus:ring-accent"
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="w-full py-2 bg-primary text-white rounded-xl hover:bg-primary/90 transition cursor-pointer"
+                    >
+                        Register
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+}
