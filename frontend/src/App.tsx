8530b9e178a7fc794c272eade96aeca0aeec1bef
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2025-11-23 20:08:08 +0000

    removed test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,11 @@
-import { useEffect } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Nav from "./components/nav";
 import Register from './pages/register';
-import axiosClient from './axios-client';
+
 
 function App() {
-  useEffect(() => {
-    axiosClient.get<string>("/health")
-      .then(res => res)
-      .then(data => console.log(data.data))
-      .catch(err => console.error(err));
-  }, []);
   return (
     <>
       <BrowserRouter>
